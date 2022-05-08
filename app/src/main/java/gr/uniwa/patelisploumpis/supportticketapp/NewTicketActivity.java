6d4d4d2e5200f769299ac77140b866b0de7d0d30
Vasilis Patelis <65589110+VasilisPat@users.noreply.github.com>
2022-05-09 00:29:05 +0300

    Added Save and Cancel Button Listeners

--- app/src/main/java/gr/uniwa/patelisploumpis/supportticketapp/NewTicketActivity.java
+++ app/src/main/java/gr/uniwa/patelisploumpis/supportticketapp/NewTicketActivity.java
@@ -2,6 +2,7 @@ package gr.uniwa.patelisploumpis.supportticketapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
@@ -9,12 +10,14 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 public class NewTicketActivity extends AppCompatActivity {
 
-    private Spinner spinner;
-    private String workType;
+    private Button cancelButton, saveButton;
+    private Spinner laborTypeSpinner;
+    private String laborType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,21 +25,45 @@ public class NewTicketActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_ticket);
 
+        cancelButton = findViewById(R.id.button_cancel_ticket);
+        saveButton = findViewById(R.id.button_save_ticket);
+        laborTypeSpinner = (Spinner) findViewById(R.id.spinner_labor_type);
+
         //Colorize action bar
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#FF5131")));
 
         //Fill spinner with options
-        spinner = (Spinner) findViewById(R.id.spinner_type);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.type_work, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.labor_type, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
+        laborTypeSpinner.setAdapter(adapter);
 
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        //Spinner item selection listener
+        laborTypeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
                 Log.i("INFO", "Spinner's onItemSelected Listener called");
-                workType = parent.getItemAtPosition(pos).toString();
+                laborType = parent.getItemAtPosition(pos).toString();
             }
             public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("INFO", "Save ticket button onClick method called");
+                //TODO 2.1 Get text from all edit views
+                //TODO 2.2 Toast message success/fail
+                Intent intent = new Intent(NewTicketActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("INFO", "Cancel ticket button onClick method called");
+                Intent intent = new Intent(NewTicketActivity.this, MainActivity.class);
+                startActivity(intent);
             }
         });
     }
